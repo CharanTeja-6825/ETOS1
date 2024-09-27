405f573a0ef11ea2ec96f090a179e7cd8e489744
shankar <pillyigshankar@gmail.com>
2024-09-27 16:01:30 +0530

    change{

--- employee/urls.py
+++ employee/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
   path('profile/update/', views.update_profile, name='update_profile'),
   path('profile/password/', views.update_password, name='update_password'),
   path('profile/', views.profile_page, name='profile_page'),
-
 ]
