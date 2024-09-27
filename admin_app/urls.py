9f744eaeb4adfdf730f909d5b8df652d9b59ec79
CHARAN TEJA RATHIKINDI <163865186+CharanTeja-6825@users.noreply.github.com>
2024-09-27 16:55:20 +0530

    Delete Course Management urls, views, and html files

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -12,10 +12,5 @@ urlpatterns = [
     path('profile/', views.profile_page, name='profile_page'),
     path('profile/update/', views.update_profile, name='update_profile'),
     path('profile/password/', views.update_password, name='update_password'),
-    path('courses/', views.course_list_view, name='course-list'),
-    path('courses/new/', views.create_course, name='course-create'),
-    path('courses/<int:pk>/edit/', views.course_update_view, name='course-update'),
-    path('courses/<int:pk>/delete/', views.course_delete_view, name='course-delete'),
-    path('courses/<int:course_id>/modules/new/', views.create_module, name='create-module'),
-    path('courses/<int:course_id>/modules/<int:module_id>/edit/', views.edit_module, name='edit-module'),
+
 ]
