0a3f1c165ae7e7a47ce6af83e5cf7aa3e4610d21
CHARAN TEJA RATHIKINDI <163865186+CharanTeja-6825@users.noreply.github.com>
2024-09-28 22:16:29 +0530

    Added User Model

--- admin_app/models.py
+++ admin_app/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.PositiveIntegerField()  # Duration in hours
+    trainer = models.ForeignKey(User, limit_choices_to={'username__length': 4}, on_delete=models.SET_NULL, null=True)  # Trainer with username length 4
+    status = models.BooleanField(default=True)  # Active/Inactive status
+
+    def __str__(self):
+        return self.name
