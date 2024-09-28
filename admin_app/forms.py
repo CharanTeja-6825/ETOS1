0a2b6e5dd17558263e5d4ec5573358dd8d1a81a5
CHARAN TEJA RATHIKINDI <163865186+CharanTeja-6825@users.noreply.github.com>
2024-09-28 22:20:10 +0530

    Added Course Form

--- admin_app/forms.py
+++ admin_app/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django import forms
 from django.contrib.auth.models import User
 from django import forms
+from .models import Course
 
 class UpdatePasswordForm(PasswordChangeForm):
     old_password = forms.CharField(widget=forms.PasswordInput())
@@ -12,3 +13,12 @@ class UpdatePasswordForm(PasswordChangeForm):
         model = User
         fields = ['old_password', 'new_password1', 'new_password2']
 
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['name', 'description', 'duration', 'trainer', 'status']
+
+    def __init__(self, *args, **kwargs):
+        super(CourseForm, self).__init__(*args, **kwargs)
+        # Filter trainers with username length of 4
+        self.fields['trainer'].queryset = User.objects.filter(username__length=4)
