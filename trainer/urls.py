d6cfa0a2e75d8120875301edeb57a590ba6de372
CHARAN TEJA RATHIKINDI <163865186+CharanTeja-6825@users.noreply.github.com>
2024-09-30 15:48:19 +0530

    Urls Trainer

--- trainer/urls.py
+++ trainer/urls.py
@@ -4,8 +4,5 @@ from . import views
 app_name = 'trainer'
 
 urlpatterns = [
-    path('homepage_trainer/', views.homepage_trainer, name='homepage_trainer'),
-    path('trainer/profile/', views.trainer_profile, name='trainer_profile'),
-    path('trainer/assigned_employees/', views.assigned_employees, name='assigned_employees'),
-    path('trainer/progress_report/<int:employee_id>/', views.progress_report, name='progress_report'),
+
 ]
